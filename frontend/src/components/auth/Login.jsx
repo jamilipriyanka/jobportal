a57d3ccb3d597b471b08f15972353c52e6e6137f
jamilipriyanka <154789860+jamilipriyanka@users.noreply.github.com>
2025-04-16 19:54:02 +0530

    Login.jsx

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
         e.preventDefault();
         try {
             dispatch(setLoading(true));
-            const res = await axios.post("https://jobportal-fy20.onrender.com/api/v1/user/login", input, {
+            const res = await axios.post("https://jobportal-fy20.onrender.com/login", input, {
                 headers: {
                     "Content-Type": "application/json"
                 },
